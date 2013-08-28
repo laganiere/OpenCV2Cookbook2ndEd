ee881799e7808887e02194945622a9ef1857032b
Robert Laganiere <robert@laganiere.name>
2013-08-28 15:26:21 -0400

    chap 11 added

--- Chapter11/foreground.cpp
+++ Chapter11/foreground.cpp
@@ -1,8 +1,7 @@
 /*------------------------------------------------------------------------------------------*\
    This file contains material supporting chapter 10 of the cookbook:  
-   Computer Vision Programming using the OpenCV Library 
-   Second Edition 
-   by Robert Laganiere, Packt Publishing, 2013.
+   Computer Vision Programming using the OpenCV Library. 
+   by Robert Laganiere, Packt Publishing, 2011.
 
    This program is free software; permission is hereby granted to use, copy, modify, 
    and distribute this source code, or portions thereof, for any purpose, without fee, 
@@ -13,58 +12,38 @@
    The author disclaims all warranties with regard to this software, any use, 
    and any consequent failure, is purely the responsibility of the user.
  
-   Copyright (C) 2013 Robert Laganiere, www.laganiere.name
+   Copyright (C) 2010-2011 Robert Laganiere, www.laganiere.name
 \*------------------------------------------------------------------------------------------*/
 
-#include <string>
 #include <iostream>
-#include <sstream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/background_segm.hpp>
 
 #include "videoprocessor.h"
-
-void draw(const cv::Mat& img, cv::Mat& out) {
-
-	img.copyTo(out);
-	cv::circle(out, cv::Point(100,100),5,cv::Scalar(255,0,0),2);
-}
-
-// processing function
-void canny(cv::Mat& img, cv::Mat& out) {
-
-   // Convert to gray
-   if (img.channels()==3)
-      cv::cvtColor(img,out,CV_BGR2GRAY);
-   // Compute Canny edges
-   cv::Canny(out,out,100,200);
-   // Invert the image
-   cv::threshold(out,out,128,255,cv::THRESH_BINARY_INV);
-}
+#include "BGFGSegmentor.h"
 
 int main()
 {
 	// Open the video file
-    cv::VideoCapture capture("bike.avi");
+    cv::VideoCapture capture("../bike.avi");
 	// check if video successfully opened
 	if (!capture.isOpened())
-		return 1;
+		return 0;
 
-	// Get the frame rate
-	double rate= capture.get(CV_CAP_PROP_FPS);
-	std::cout << "Frame rate: " << rate << "fps" << std::endl;
+	// current video frame
+	cv::Mat frame; 
+	// foreground binary image
+	cv::Mat foreground;
+
+	cv::namedWindow("Extracted Foreground");
+
+	// The Mixture of Gaussian object
+	// used with all default parameters
+	cv::BackgroundSubtractorMOG mog;
 
 	bool stop(false);
-	cv::Mat frame; // current video frame
-	cv::namedWindow("Extracted Frame");
-
-	// Delay between each frame
-	// corresponds to video frame rate
-	int delay= 1000/rate;
-	long long i=0;
-	std::string b="bike";
-	std::string ext=".bmp";
 	// for all frames in video
 	while (!stop) {
 
@@ -72,47 +51,45 @@ int main()
 		if (!capture.read(frame))
 			break;
 
-		cv::imshow("Extracted Frame",frame);
+		// update the background
+		// and return the foreground
+		mog(frame,foreground,0.01);
 
-		std::string name(b);
-		name+=std::to_string(i++);
-		name+=ext;
+		// Complement the image
+		cv::threshold(foreground,foreground,128,255,cv::THRESH_BINARY_INV);
 
-		std::cout << name <<std::endl;
-		
-		cv::imwrite(name,frame);
+		// show foreground
+		cv::imshow("Extracted Foreground",foreground);
 
 		// introduce a delay
 		// or press key to stop
-		if (cv::waitKey(delay)>=0)
+		if (cv::waitKey(10)>=0)
 				stop= true;
 	}
 
-	// Close the video file
-	capture.release();
-
 	cv::waitKey();
 
-	// Now using the VideoProcessor class
-
-	// Create instance
+	// Create video procesor instance
 	VideoProcessor processor;
 
+	// Create background/foreground segmentor 
+	BGFGSegmentor segmentor;
+	segmentor.setThreshold(25);
+
 	// Open video file
-	processor.setInput("bike.avi");
+	processor.setInput("../bike.avi");
+
+	// set frame processor
+	processor.setFrameProcessor(&segmentor);
 
 	// Declare a window to display the video
-	processor.displayInput("Input Video");
-	processor.displayOutput("Output Video");
+	processor.displayOutput("Extracted Foreground");
 
 	// Play the video at the original frame rate
 	processor.setDelay(1000./processor.getFrameRate());
 
-	// Set the frame processor callback function
-	processor.setFrameProcessor(canny);
-
 	// Start the process
 	processor.run();
 
-	cv::waitKey();	
+	cv::waitKey();
 }
